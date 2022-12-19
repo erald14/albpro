8f7483119238105decc282adcc1e2ba299e5ec43
Erald Calaj <erald.calaj@quickbirdstudios.com>
2022-12-20 00:26:08 +0100

    feat: remove lint warning

--- next.config.js
+++ next.config.js
@@ -21,7 +21,7 @@ module.exports = {
       return [
         {
           source: '/api/:path*',
-          destination: 'https://api.example.com/:path*',
+          destination: 'https://albpro.vercel.app/:path*',
         },
       ]
     },
